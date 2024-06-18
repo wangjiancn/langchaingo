b286844bdae038073d6ed4a939988a15da24d0ac
wangjian <wangjianchn@outlook.com>
2024-06-18 16:44:01 +0800

    test: add test case for convertTools

--- llms/googleai/shared_test/shared_test.go
+++ llms/googleai/shared_test/shared_test.go
@@ -100,12 +100,17 @@ var testConfigs = []testConfig{
 	{testWithHTTPClient, getHTTPTestClientOptions()},
 }
 
-func TestShared(t *testing.T) {
+func TestGoogleAIShared(t *testing.T) {
 	for _, c := range testConfigs {
 		t.Run(fmt.Sprintf("%s-googleai", funcName(c.testFunc)), func(t *testing.T) {
 			llm := newGoogleAIClient(t, c.opts...)
 			c.testFunc(t, llm)
 		})
+	}
+}
+
+func TestVertexShared(t *testing.T) {
+	for _, c := range testConfigs {
 		t.Run(fmt.Sprintf("%s-vertex", funcName(c.testFunc)), func(t *testing.T) {
 			llm := newVertexClient(t, c.opts...)
 			c.testFunc(t, llm)
@@ -134,6 +139,8 @@ func testMultiContentText(t *testing.T, llm llms.Model) {
 	assert.NotEmpty(t, rsp.Choices)
 	c1 := rsp.Choices[0]
 	assert.Regexp(t, "(?i)dog|carnivo|canid|canine", c1.Content)
+	assert.Contains(t, rsp.Choices[0].GenerationInfo, "output_tokens")
+	assert.NotZero(t, rsp.Choices[0].GenerationInfo["output_tokens"])
 }
 
 func testMultiContentTextUsingTextParts(t *testing.T, llm llms.Model) {
@@ -317,6 +324,8 @@ func testWithStreaming(t *testing.T, llm llms.Model) {
 	c1 := rsp.Choices[0]
 	assert.Regexp(t, "dog|canid", strings.ToLower(c1.Content))
 	assert.Regexp(t, "dog|canid", strings.ToLower(sb.String()))
+	assert.Contains(t, rsp.Choices[0].GenerationInfo, "output_tokens")
+	assert.NotZero(t, rsp.Choices[0].GenerationInfo["output_tokens"])
 }
 
 func testTools(t *testing.T, llm llms.Model) {
@@ -337,10 +346,30 @@ func testTools(t *testing.T, llm llms.Model) {
 							"description": "The city and state, e.g. San Francisco, CA",
 						},
 					},
-					"required": []string{"location"},
+					// json.Unmarshal() may return []interface{} instead of []string
+					"required": []interface{}{"location"},
 				},
 			},
 		},
+		// don't test this one, gemini doesn't support multiple tools
+		//{
+		//	Type: "function",
+		//	Function: &llms.FunctionDefinition{
+		//		Name:        "getIpInfo",
+		//		Description: "Get information about an IP address",
+		//		Parameters: map[string]any{
+		//			"type": "object",
+		//			"properties": map[string]any{
+		//				"ip": map[string]any{
+		//					"type":        "string",
+		//					"description": "The IP address to look up",
+		//				},
+		//			},
+		//			// json.Unmarshal() may return []interface{} instead of []string
+		//			"required": []string{"ip"},
+		//		},
+		//	},
+		//},
 	}
 
 	content := []llms.MessageContent{
@@ -397,6 +426,8 @@ func testTools(t *testing.T, llm llms.Model) {
 
 	c1 = resp.Choices[0]
 	assert.Regexp(t, "64 and sunny", strings.ToLower(c1.Content))
+	assert.Contains(t, resp.Choices[0].GenerationInfo, "output_tokens")
+	assert.NotZero(t, resp.Choices[0].GenerationInfo["output_tokens"])
 }
 
 func testMaxTokensSetting(t *testing.T, llm llms.Model) {
